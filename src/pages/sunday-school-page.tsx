de14dd23359ced8e657dac5052bc5262acf96410
Alex Teebagy <alexteebagy@Alexs-MacBook-Pro.local>
2023-08-27 17:06:18 -0500

    Sunday School: Initial setup of page

--- src/pages/sunday-school-page.tsx
+++ src/pages/sunday-school-page.tsx
@@ -1,7 +1,35 @@
 export const SundaySchool = () => {
   return (
     <div>
-      <h1>Sunday School</h1>
-    </div>
+      <body>
+        <div id="SundaySchoolSummary">
+          <h1 id="SundaySchoolHeader">Sunday School</h1>
+          <p>Let's learn Arabic, Islamic studies, and Quran at Sunday School</p>
+        </div>
+        <div id="TeacherInfoSection">
+          <h2 id="TeacherInfoHeader">Teacher Information</h2>
+          <img id="TeacherImage" src="teacherimage.jpg"/>
+          <p id="TeacherBio">Ms. Valerie Frizzle is a phenomenal teacher that has been working within the program for 20 years</p>
+        </div>
+        <div id="CharterRulesSection">
+          <h2 id="CharterRulesHeader">Charter Rules</h2>
+          <p>
+            The rules of Sunday School are:
+            <ul>
+              <li>Respect your teacher</li>
+              <li>Some other charter rules</li>
+            </ul>
+          </p>
+        </div>
+        <div id="CostSection">
+          <h2 id="CostHeader">Dues/Cost</h2>
+          <p>Tuition for the program is: </p>
+        </div>
+        <div id="TimingsSection">
+          <h2 id="TimingsHeader">Timings</h2>
+          <p>Classes are held on Sundays from start time to end time</p>
+        </div>
+    </body>
+  </div>
   );
 };
