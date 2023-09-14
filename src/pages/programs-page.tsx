28c1016c828f56538b0fd851b9d5123cf2169809
Shaji S <shajisami@gmail.com>
2023-09-13 23:19:19 -0400

    add programs page

--- src/pages/programs-page.tsx
+++ src/pages/programs-page.tsx
@@ -1,5 +1,10 @@
+import ProgramSelectMenu from '../components/ProgramSelectMenu';
 import Page from '../layout/Page';
 
 export const Programs = () => {
-  return <Page headerText='PROGRAMS' isUnderConstruction></Page>;
+  return (
+    <Page headerBody='MCNET provides a number of classes and/or programs; please see below.' headerText='PROGRAMS'>
+      <ProgramSelectMenu />
+    </Page>
+  );
 };
